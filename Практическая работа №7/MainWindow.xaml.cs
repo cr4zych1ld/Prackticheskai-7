e17bd387c3128792269022b66a9c9cf49a3a9e91
artem <artem@DESKTOP-RRLJ258>
2024-11-07 18:03:24 +0300

    Добавьте файлы проекта.

---
+++ Практическая работа №7/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Практическая_работа__7
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void btnInfoProga_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Задание: Использовать класс Pair (пара чисел). \n" +
+                "Определить класс-наследник Fraction с харак-\nтристиками: целая часть числа и дробная \n" +
+                "часть числа.Определить операцию сложения \nдвух денежных едениц.", "О программе");
+        }
+
+        private void btnInfoSozdatel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ФИО: Жаров Артём Андреевич \nСтудент группы ИСП-31", "О создателе");
+        }
+
+        private void btnPrimechanie_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Вводите пару чисел через знак: 'точку с запятой' ; ", "Примечание");
+        }
+
+        private void btnRez_Click(object sender, RoutedEventArgs e)
+        {
+            double firstElementP1 = 0;
+            double secondElementP1 = 0;
+            string InputFirstPara = FirstParaChisel.Text;
+            string[] ElementsFirstPara = InputFirstPara.Split(';');
+
+            double firstElementP2 = 0;
+            double secondElementP2 = 0;
+            string InputSecondPara = SecondParaChisel.Text;
+            string[] ElementsSecondPara = InputSecondPara.Split(";");
+
+            if (ElementsFirstPara.Length == 2 || ElementsSecondPara.Length == 2)
+            {
+                if (double.TryParse(ElementsFirstPara[0].Trim(), out firstElementP1) && double.TryParse(ElementsFirstPara[1].Trim(), out secondElementP1) && double.TryParse(ElementsSecondPara[0].Trim(), out firstElementP2) && double.TryParse(ElementsSecondPara[1].Trim(), out secondElementP2))
+                {
+                    Pair pair1 = new Pair(firstElementP1, secondElementP1);
+                    Pair pair2 = new Pair(firstElementP2, secondElementP2);
+
+                    if (pair1 > pair2)
+                    {
+                        tbRezult.Text = "Первая пара чисел больше второй пары чисел.";
+                    }
+                    else if (pair1 < pair2)
+                    {
+                        tbRezult.Text = "Первая пара чисел меньше второй пары чисел.";
+                    }
+                    else
+                    {
+                        tbRezult.Text = "Первая пара чисел равна второй паре чисел.";
+                    }
+                }
+                else tbRezult.Text = "Ошибка: введены некорректные значения!";
+            }
+            else tbRezult.Text = "Ошибка: введите два числа в каждую пару чисел!";
+        }
+
+        private void btnSumm_Click(object sender, RoutedEventArgs e)
+        {
+            double firstElementP1 = 0;
+            double secondElementP1 = 0;
+            string InputFirstPara = FirstParaChisel.Text;
+            string[] ElementsFirstPara = InputFirstPara.Split(';');
+
+            double firstElementP2 = 0;
+            double secondElementP2 = 0;
+            string InputSecondPara = SecondParaChisel.Text;
+            string[] ElementsSecondPara = InputSecondPara.Split(";");
+
+            if (ElementsFirstPara.Length == 2 || ElementsSecondPara.Length == 2)
+            {
+                if (double.TryParse(ElementsFirstPara[0].Trim(), out firstElementP1) && double.TryParse(ElementsFirstPara[1].Trim(), out secondElementP1) && double.TryParse(ElementsSecondPara[0].Trim(), out firstElementP2) && double.TryParse(ElementsSecondPara[1].Trim(), out secondElementP2))
+                {
+                    Fraction f1 = new Fraction(firstElementP1, secondElementP1);
+                    Fraction f2 = new Fraction(firstElementP2, secondElementP2);
+
+                    Fraction rezult = f1 + f2;
+
+                    tbRezult.Text = $"{rezult}";
+                }
+            }
+        }
+    }
+}
---
+++ Практическая работа №7/Pair.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Практическая_работа__7
+{
+    /// <summary>
+    /// Класс для сравнения пар чисел с использованием операторов
+    /// </summary>
+    public class Pair
+    {
+        /// <summary>
+        /// Автоматическое свойство для первого числа пары
+        /// </summary>
+        public double First { get; set; }
+        /// <summary>
+        /// Автоматическое свойство для второго числа пары
+        /// </summary>
+        public double Second { get; set; }
+
+        /// <summary>
+        /// Параметризованный конструктор для класс Pair
+        /// </summary>
+        /// <param name="first">Первое число в паре чисел</param>
+        /// <param name="second">Второе число в паре чисел</param>
+        public Pair(double first, double second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        /// <summary>
+        /// Метод для установки параметров объекта
+        /// </summary>
+        /// <param name="first">Первое число в паре чисел</param>
+        /// <param name="second">Второе число в паре чисел</param>
+        public void SetParams(double first, double second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        /// <summary>
+        /// Перегруженный метод для установки параметров объекта
+        /// </summary>
+        /// <param name="pair">Переменная обозначающая пару чисел</param>
+        public void SetParams(Pair pair)
+        {
+            First = pair.First;
+            Second = pair.Second;
+        }
+
+        /// <summary>
+        /// Перегруженный оператор для сравнения пар со знаком "больше"
+        /// </summary>
+        /// <param name="p1">Переменная обозначающая первую пару чисел</param>
+        /// <param name="p2">Переменная обозначающая вторую пару чисел</param>
+        /// <returns>Результата сравнения пар</returns>
+        public static bool operator >(Pair p1, Pair p2)
+        {
+            return (p1.First > p2.First) || (p1.First == p2.First && p1.Second > p2.Second);
+        }
+
+        /// <summary>
+        /// Перегруженный оператор для сравнения пар со знаком "меньше"
+        /// </summary>
+        /// <param name="p1">Переменная обозначающая первую пару чисел</param>
+        /// <param name="p2">Переменная обозначающая вторую пару чисел</param>
+        /// <returns>Результат сравнения пар</returns>
+        public static bool operator <(Pair p1, Pair p2)
+        {
+            return (p1.First < p2.First) || (p1.First == p2.First && p1.Second < p2.Second);
+        }
+
+        /// <summary>
+        /// Перегруженный оператор для сравнения пар со знаком "равно"
+        /// </summary>
+        /// <param name="p1">Переменная обозначающая первую пару чисел</param>
+        /// <param name="p2">Переменная обозначающая вторую пару чисел</param>
+        /// <returns>Результат сравнения пар</returns>
+        public static bool operator ==(Pair p1, Pair p2)
+        {
+            return p1.First == p2.First && p1.Second == p2.Second;
+        }
+
+        /// <summary>
+        /// Перегруженный оператор для сравнения пар со знаком "не равно"
+        /// </summary>
+        /// <param name="p1">Переменная обозначающая первую пару чисел</param>
+        /// <param name="p2">Переменная обозначающая вторую пару чисел</param>
+        /// <returns>Результат сравнения пар</returns>
+        public static bool operator !=(Pair p1, Pair p2)
+        {
+            return !(p1 == p2);
+        }
+    }
+
+    public class Fraction : Pair
+    {
+        /// <summary>
+        /// Параметризированный конструктор
+        /// </summary>
+        /// <param name="first">Первое число в паре чисел</param>
+        /// <param name="second">Второе число в паре чисел</param>
+        public Fraction(double first, double second) : base(first, second) { }
+
+        /// <summary>
+        /// Перегруженный оператор для сложения пар чисел со знаком "плюс"
+        /// </summary>
+        /// <param name="p1">Переменная обозначающая первую пару чисел</param>
+        /// <param name="p2">Переменная обозначающая вторую пару чисел</param>
+        /// <returns>Вывод новой пары чисел со сложенными значениями</returns>
+        public static Fraction operator +(Fraction p1, Fraction p2)
+        {
+            double first = p1.First + p2.First;
+            double second = p1.Second + p2.Second;
+
+            if(second >= 1)
+            {
+                first = first + 1;
+                second = Math.Round((second - 1), 2);
+            }
+
+            return new Fraction(first, second);
+        }
+
+        /// <summary>
+        /// Переопределение метода
+        /// </summary>
+        /// <returns>Вывод значений в стринговом типе</returns>
+        public override string ToString()
+        {
+            return $"{First};{Second}";
+        }
+    }
+}
